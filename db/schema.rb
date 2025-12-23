6f642941604a2417969814b26cc69f558360d5d5
Arne De Herdt <arne.de.herdt@gmail.com>
2025-12-23 14:13:22 +0100

    Add the `schema.rb` changes.

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 0) do
+ActiveRecord::Schema[8.1].define(version: 2025_12_23_124351) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
+  create_table "users", force: :cascade do |t|
+    t.boolean "activated", default: false, null: false
+    t.datetime "activated_at"
+    t.datetime "created_at", null: false
+    t.string "email", null: false
+    t.string "first_name"
+    t.string "last_name"
+    t.string "nickname"
+    t.string "password_digest"
+    t.datetime "updated_at", null: false
+  end
 end
